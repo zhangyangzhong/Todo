bace050d8dbeea583acf98d06659739ff1b1cf13
Dennis <ococo09000@gmail.com>
2021-06-05 00:56:54 +0800

    publish

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React,{ useRef, useState, } from 'react'
+import React,{ useRef, useState, useEffect} from 'react'
 
 function App() {
   const [arr, setArr] = useState([]); 
   const [text, setText] = useState('');
 
+
+  const removeFN = (index) =>{
+    console.log(index)
+    setArr(function(prev){  //透過獲取index值,並且使用一個function取得oldValue
+      return prev.filter((item,id) => id !== index)  //if id = false  return 
+    })
+  }
+
   const AddFn = () =>{
-    setArr(() =>[...arr,text]); //need to copy the arr,sure the data be saved
+    setArr((prev) =>[...prev, text]); //need to copy the arr,sure the data be saved
     setText("") //and clear the input data
   }
+  
   return (
     <div>
       <h1>Todo List</h1>
@@ -17,13 +26,16 @@ function App() {
         value={text} 
         onChange={(event)=>setText(event.target.value)} 
       />
+      <button onClick={AddFn}>Add</button>
       <p>{text}</p>
       <br />
-      <ul>
-        {arr.map((item) => <li key={item.toString()}>{item}<br/></li>)}
+      <ul>    
+        {arr.map((item,index) => <div key={item.toString()} style={{display:'flex'}}>
+            <li>{index +1}.{item}</li>
+            <button onClick={()=> removeFN(index)}>-</button>
+          </div>)
+        }      
       </ul>
-      <button onClick={AddFn}>Add</button>
-      <p></p>
     </div>
   )
 }
