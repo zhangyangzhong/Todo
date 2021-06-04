32b412fd0a25847f11ec70836691f36b7acbf91f
Dennis <ococo09000@gmail.com>
2021-06-04 21:34:36 +0800

    Todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useRef, useState, } from 'react'
 
 function App() {
+  const [arr, setArr] = useState([]); 
+  const [text, setText] = useState('');
+
+  const AddFn = () =>{
+    setArr(() =>[...arr,text]); //need to copy the arr,sure the data be saved
+    setText("") //and clear the input data
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+      <input 
+        type="text" 
+        placeholder="Input wanna do thing " 
+        value={text} 
+        onChange={(event)=>setText(event.target.value)} 
+      />
+      <p>{text}</p>
+      <br />
+      <ul>
+        {arr.map((item) => <li key={item.toString()}>{item}<br/></li>)}
+      </ul>
+      <button onClick={AddFn}>Add</button>
+      <p></p>
     </div>
-  );
+  )
 }
 
-export default App;
+
+
+export default App
